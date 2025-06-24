2825ab3cbac346ee818afcaa0d2dc734f93657cc
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-06-24 11:44:27 -0300

    animando fundo mudando de cor ao acertar/errar quiz

--- src/components/OverlayFeedback/index.tsx
+++ src/components/OverlayFeedback/index.tsx
@@ -1,7 +1,14 @@
-import Animated from 'react-native-reanimated';
+import Animated, {
+    Easing,
+    useSharedValue,
+    withSequence,
+    withTiming,
+    useAnimatedStyle
+} from 'react-native-reanimated';
 import {useWindowDimensions} from "react-native";
 import {Canvas, Rect, BlurMask} from '@shopify/react-native-skia';
 import {THEME} from "../../styles/theme";
+import {useEffect} from "react";
 
 const STATUS = ['transparent', THEME.COLORS.BRAND_LIGHT, THEME.COLORS.DANGER_LIGHT];
 
@@ -10,13 +17,26 @@ type Props = {
 }
 
 export function OverlayFeedback({status} : Props){
-
+    const opacity = useSharedValue(0);
     const color = STATUS[status];
 
     const {height, width} = useWindowDimensions();
 
+    const styleAnimated = useAnimatedStyle(() => {
+        return{
+            opacity: opacity.value,
+        }
+    })
+
+    useEffect(() => {
+        opacity.value = withSequence(
+            withTiming(1, {duration: 400, easing: Easing.bounce}),
+            withTiming(0)
+        );
+    }, [status]);
+
     return(
-        <Animated.View style={{height, width, position: "absolute"}} >
+        <Animated.View style={[{height, width, position: "absolute"}, styleAnimated]} >
             <Canvas style={{flex: 1}}>
                 <Rect x={0} y={0} width={width} height={height} color={color}>
                     <BlurMask blur={50} style="inner"/>
